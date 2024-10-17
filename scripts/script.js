4eb681fe2f5be915156c75b9581bb25b2f5f6507
ambersr <amber.schalker@hva.nl>
2024-10-17 14:54:49 +0200

    JS menu geveltuin

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Code voor openklappen hamburger menu in nav
+
+const menuButton = document.querySelector("#menuButton");
+const hetMenu = document.querySelector("#menu");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    hetMenu.classList.toggle("open");
+}
+
